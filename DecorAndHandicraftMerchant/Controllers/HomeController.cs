1af180677efed9cb4bf7df4dd845f3a2ef2edcb2
Mohpreet Kaur <67534712+mohpreetk@users.noreply.github.com>
2021-02-19 23:19:38 -0500

    Update to general layout, privacy and return policy.

--- DecorAndHandicraftMerchant/Controllers/HomeController.cs
+++ DecorAndHandicraftMerchant/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace DecorAndHandicraftMerchant.Controllers
             return View();
         }
 
+        public IActionResult Returns()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
