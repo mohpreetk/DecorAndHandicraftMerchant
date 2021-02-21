101b8c3422e51b5e67201c45bdb714d440321cba
Mohpreet Kaur <67534712+mohpreetk@users.noreply.github.com>
2021-02-20 23:51:07 -0500

    Added Controllers and Views. Connected Categories, Subcategories and Products using ViewBag

---
+++ DecorAndHandicraftMerchant/Controllers/SubCategoriesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DecorAndHandicraftMerchant.Data;
+using DecorAndHandicraftMerchant.Models;
+
+namespace DecorAndHandicraftMerchant.Controllers
+{
+    public class SubCategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SubCategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SubCategories
+        public async Task<IActionResult> Index(int id)
+        {
+            //passing on the category id to SubCategory Page
+            ViewBag.id = id;
+            var applicationDbContext = _context.SubCategories.Include(s => s.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: SubCategories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await _context.SubCategories
+                .Include(s => s.Category)
+                .FirstOrDefaultAsync(m => m.SubCategoryId == id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name");
+            return View();
+        }
+
+        // POST: SubCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SubCategoryId,Name,CategoryId")] SubCategory subCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(subCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name", subCategory.CategoryId);
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await _context.SubCategories.FindAsync(id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name", subCategory.CategoryId);
+            return View(subCategory);
+        }
+
+        // POST: SubCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SubCategoryId,Name,CategoryId")] SubCategory subCategory)
+        {
+            if (id != subCategory.SubCategoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(subCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SubCategoryExists(subCategory.SubCategoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name", subCategory.CategoryId);
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await _context.SubCategories
+                .Include(s => s.Category)
+                .FirstOrDefaultAsync(m => m.SubCategoryId == id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subCategory);
+        }
+
+        // POST: SubCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var subCategory = await _context.SubCategories.FindAsync(id);
+            _context.SubCategories.Remove(subCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SubCategoryExists(int id)
+        {
+            return _context.SubCategories.Any(e => e.SubCategoryId == id);
+        }
+    }
+}
