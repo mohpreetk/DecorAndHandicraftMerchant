fb890540133d97772e23b6b970b44c750d04ad4d
Mohpreet Kaur <67534712+mohpreetk@users.noreply.github.com>
2021-02-22 21:42:59 -0500

    Server Deployment - Website Live on Azure

---
+++ DecorAndHandicraftMerchant/Data/Migrations/20210223023951_ConnectedToAzure5.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DecorAndHandicraftMerchant.Data.Migrations
+{
+    public partial class ConnectedToAzure5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
